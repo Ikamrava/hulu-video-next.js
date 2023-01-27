df64bad129ce7fbbbd2613b91156a5da60a063c7
ikamrava <ikamrava>
2023-01-27 14:08:11 +0000

    Finish the details page

--- pages/info/[media]/[id].js
+++ pages/info/[media]/[id].js
@@ -1,8 +1,93 @@
 import React from "react";
+import { HandThumbUpIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
+import Header from "@/components/Header";
+import Navbar from "@/components/Navbar";
 
 function info({ results }) {
   console.log(results);
-  return <div>onfoid</div>;
+  return (
+    <>
+      <Header />
+      <Navbar />
+
+      <div className=" m-10 sm:m-20 sm:flex">
+        <Image
+          className=" object-cover"
+          src={`https://image.tmdb.org/t/p/w500/${results.poster_path}`}
+          width={400}
+          height={1920}
+          alt="Poster"
+        />
+        <div className=" ml-5 text-lg">
+          <div className="">
+            <h2 className=" mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">
+              {results.title || results.original_name}
+            </h2>
+            <label className=" text-bold text-yellow-400">Overview:</label>
+            <p className=" max-w-md">{results.overview}</p>
+          </div>
+
+          <div>
+            <div className="flex ">
+              <label className=" text-bold text-yellow-400">Genres: </label>
+              {results.genres.map((genre) => (
+                <p> {genre.name} .</p>
+              ))}
+            </div>
+            <a
+              className=" text-blue-500"
+              href={results.homepage}
+              target="_blank"
+            >
+              <span className=" text-bold text-yellow-400"> Home Page: </span>
+              {results.homepage}
+            </a>
+            <p>
+              <span className=" text-bold text-yellow-400">
+                Original Language :
+              </span>{" "}
+              {results.original_language.toUpperCase()}
+            </p>
+            <p>
+              <span className=" text-bold text-yellow-400">
+                Spoken Languages :
+              </span>{" "}
+              {results.spoken_languages.map((lan) => (
+                <span>{lan.english_name} .</span>
+              ))}
+            </p>
+            <p>
+              <span className=" text-bold text-yellow-400">
+                Production Companies :
+              </span>{" "}
+              {results.production_companies.map((lan) => (
+                <span>{lan.name} .</span>
+              ))}
+            </p>
+
+            <p>
+              <span className=" text-bold text-yellow-400">RunTime: </span>
+              {results.runtime} min
+            </p>
+            <p>
+              <span className=" text-bold text-yellow-400">Vote Average: </span>
+              {results.vote_average}
+            </p>
+            <p>
+              <span className=" text-bold text-yellow-400">Status: </span>
+              {results.status}
+            </p>
+            <p className=" flex items-center  group-hover:opacity-100">
+              {results.media_type && `${Upercase(results.media_type)}`}{" "}
+              {results.release_date || results.first_air_dat}{" "}
+              <HandThumbUpIcon className=" h-5 mx-2 " /> {results.vote_count}
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default info;
