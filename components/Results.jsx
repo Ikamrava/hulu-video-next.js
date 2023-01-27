52447af67caf414877ac99cb72a952e5f50da520
ikamrava <ikamrava>
2023-01-27 14:50:55 +0000

    fix search

--- components/Results.jsx
+++ components/Results.jsx
@@ -6,7 +6,7 @@ import Info from './Info'
 
 
 function Results({results}) {
-  console.log(results)
+
   return (
     <>
     <div className='px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:grid-cols-4'>
